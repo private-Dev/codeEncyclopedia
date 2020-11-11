250a07ccf8755c405c554d990c3030104bbae57b
jpb4git <[dev@guitaresphere.com]>
2020-11-11 12:06:25 +0100

    feature persist select id on theme and blocknote

---
+++ scripts/interface_addParagraphToNote.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root-home
+ * Date: 11/11/2020
+ * Time: 11:58
+ */
+
+Session_start();
+
+if (!isset( $_SESSION['auth'])){
+    // header('Location: ../login.php');
+    exit();
+}
+include_once     "../classes/class.Constants.php";
+include_once     "../classes/db/class.Database.php";
+include_once     "../classes/metier/theme.class.php";
+include_once     "../classes/metier/blocknote.class.php";
+include_once     "../classes/metier/note.class.php";
+include_once     "../classes/user/class.User.php";
+
+
+
+$action = isset($_POST['action']) ? $_POST['action']:'';
+$fktheme = isset($_POST['fktheme']) ? $_POST['fktheme']:'';
+$fkblocknote = isset($_POST['fkblocknote']) ? $_POST['fkblocknote']:'';
+$notelabel = isset($_POST['notelabel']) ? $_POST['notelabel']:'';
+$paragraph = isset($_POST['paragraph']) ? $_POST['paragraph']:'';
+
+
+if (isset($action) && !empty($action) &&  $action == 'addnote'){
+
+    // create note attached to blockNote
+
+    // get lastId note
+
+    // create paragraph attached to this note
+var_dump($action,$fktheme,$fkblocknote,$notelabel,$paragraph);
+exit();
+
+}
--- scripts/interface_select_blocknote.php
+++ scripts/interface_select_blocknote.php
@@ -11,15 +11,22 @@
 Session_start();
 
 if (!isset( $_SESSION['auth'])){
-    // header('Location: ../login.php');
-    //exit();
+     header('Location: ../login.php');
+    exit();
 }
 include_once     "../classes/class.Constants.php";
 include_once     "../classes/db/class.Database.php";
 include_once     "../classes/metier/blocknote.class.php";
 include_once     "../classes/user/class.User.php";
 
-$idTheme = isset($_POST['idTheme']) ? $_POST['idTheme'] :'';
+
+
+$_SESSION['NewNote']['idTheme'] = isset($_POST['idTheme']) ? $_POST['idTheme'] : '';
+if (isset($_SESSION['NewNote']['idBlock'])){
+    unset($_SESSION['NewNote']['idBlock']);
+}
+
+
 $action = isset($_POST['action']) ? $_POST['action']:'';
 
 $data =[];
@@ -27,12 +34,16 @@ $data =[];
 if (isset($action) && !empty($action) &&  $action == 'selectChangeTheme'){
     $db = new Database();
     $blocknote = new Blocknote($db->getInstance());
-    $blocknotes = $blocknote->getRows($_SESSION['auth']->id,$idTheme);
+    $blocknotes = $blocknote->getRows($_SESSION['auth']->id,$_SESSION['NewNote']['idTheme']);
 
     $html = '<label for="selectBlock" class="mt-1">Blocknote</label>';
     $html .='<select id="selectBlock" name="selectBlock" class="form-control ml-5">';
                       foreach ($blocknotes as $b){
-                          $html .='<option value="'.$b->rowid.'">'.$b->label.'</option>';
+                          $html .='<option value="'.$b->rowid.'"';
+                          if ( isset($_SESSION['NewNote']['idBlock']) && !empty($_SESSION['NewNote']['idBlock']) && $_SESSION['NewNote']['idBlock'] == $b->rowid ){
+                              $html.= ' selected';
+                          }
+                          $html .= '>'.$b->label.'</option>';
                         }
     $html .='</select>';
     $html .='<a id="addBlocknote" class="nav-link" href="#"><i class="fa fa-plus-circle" aria-hidden="true"></i></a>';
