dfd047da061bc3dcb62b507b41d47d5448bc6782
jpb4git <[dev@guitaresphere.com]>
2020-11-12 08:34:10 +0100

    add try catch on PDO prepare methods

--- classes/metier/theme.class.php
+++ classes/metier/theme.class.php
@@ -18,6 +18,7 @@ public $current_user;
     public function fetch($id){
         
         $sql = "SELECT * FROM  theme as t WHERE t.id = ? ";
+        try {
         $stmt = $this->_db->prepare($sql);
         $stmt->execute([$id]);
         $row = $stmt->fetch(); 
@@ -26,6 +27,9 @@ public $current_user;
         $this->date_created = $row->date_created;
         $this->date_update = $row->date_update;
         $stmt = null;
+        }catch(Exception $e){
+            die("Oh noes! There's an error in the query!");
+        }
     }  
     
     
@@ -48,25 +52,32 @@ public $current_user;
     public function getRank($userId,$themeId){
         
         $sql = "SELECT fk_theme,rank_display FROM  theme_display_user as td WHERE td.fk_user = ? AND fk_theme = ?";
-        $stmt = $this->_db->prepare($sql);
-        $stmt->execute([$userId,$themeId]);
-        $row = $stmt->fetch(); 
-        $stmt = null;
-        return $row;
+        try {
+            $stmt = $this->_db->prepare($sql);
+            $stmt->execute([$userId,$themeId]);
+            $row = $stmt->fetch(); 
+            $stmt = null;
+            return $row;
+        }catch(Exception $e){
+            die("Oh noes! There's an error in the query!");
+        }
     }
 
     public function updateUserRank($userId,$current_theme,$rank){
         $sql="UPDATE theme_display_user   
         SET `rank_display` = :rank
         WHERE `fk_user` = :user_id  AND fk_theme = :current_theme";
-    
-         $statement = $this->_db->prepare($sql);
-    
-        $statement->bindValue(":rank", $rank);
-        $statement->bindValue(":user_id", $userId);
-        $statement->bindValue(":current_theme", $current_theme);
-    
-        $count = $statement->execute();
+        try {  
+            $statement = $this->_db->prepare($sql);
+        
+            $statement->bindValue(":rank", $rank);
+            $statement->bindValue(":user_id", $userId);
+            $statement->bindValue(":current_theme", $current_theme);
+        
+            $count = $statement->execute();
+        }catch(Exception $e){
+            die("Oh noes! There's an error in the query!");
+        }
     }
 
     public function getRows($userId){
@@ -74,45 +85,63 @@ public $current_user;
         $sql = "SELECT t.id as rowid, t.label , t.date_created,t.toolTipMsg FROM theme as t ";
         $sql .= "LEFT JOIN  theme_display_user as td ON  t.id = td.fk_theme AND td.fk_user = ? ";
         $sql .= "ORDER BY td.rank_display,t.rank";
-        $stmt = $this->_db->prepare($sql);
-        $stmt->execute([$userId]);
-        $rows = $stmt->fetchAll(); 
-        $stmt = null;
-        return $rows;
+        try {
+            $stmt = $this->_db->prepare($sql);
+            $stmt->execute([$userId]);
+            $rows = $stmt->fetchAll();     
+            $stmt = null;
+            return $rows;
+        }catch(Exception $e){
+            die("Oh noes! There's an error in the query!");
+        }
+        
+        return null;
     }
 
     public function getMaxRank(){
         $sql = "SELECT MAX(rank) as nb FROM theme";
-        $stmt = $this->_db->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->fetch(); 
-        $stmt = null;
-        return $result;
+        try {
+            $stmt = $this->_db->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetch(); 
+            $stmt = null;
+            return $result;
+        }catch(Exception $e){
+            die("Oh noes! There's an error in the query!");
+        }
     }
 
    
     public function getNbRows(){
         $sql = "SELECT count(*) as nb FROM theme";
-        $stmt = $this->_db->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->fetch(); 
-        $stmt = null;
-        return $result;
+        try {
+            $stmt = $this->_db->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetch(); 
+            $stmt = null;
+            return $result;
+        }catch(Exception $e){
+            die("Oh noes! There's an error in the query!");
+        }
     }
 
     public function  create($label,$tooltip,$rank){
 
         $date = date('Y-m-d H:i:s');
             // prepare and bind
-        $stmt = $this->_db->prepare("INSERT INTO theme (label,toolTipMsg, rank,date_created,date_update) VALUES (:label,:toolTip,:rank,:date_c,:date_u)");
-        $stmt->bindParam(':label', $label);
-        $stmt->bindParam(':toolTip', $tooltip);
-        $stmt->bindParam(':rank', $rank);
-        $stmt->bindParam(':date_c', $date);
-        $stmt->bindParam(':date_u', $date);
-        $stmt->execute();
-        $stmt = null;
+        try {    
+            $stmt = $this->_db->prepare("INSERT INTO theme (label,toolTipMsg, rank,date_created,date_update) VALUES (:label,:toolTip,:rank,:date_c,:date_u)");
+            $stmt->bindParam(':label', $label);
+            $stmt->bindParam(':toolTip', $tooltip);
+            $stmt->bindParam(':rank', $rank);
+            $stmt->bindParam(':date_c', $date);
+            $stmt->bindParam(':date_u', $date);
+            $stmt->execute();
+            $stmt = null;
         return $this->_db->lastInsertId(); 
+        }catch(Exception $e){
+            die("Oh noes! There's an error in the query!");
+         }
     }
 }
 
